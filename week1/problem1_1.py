69b730d6bb4cd0185729262647de971b187fa8bb
M.hadiyaqoobi <41824176+Hadiyaqoobi@users.noreply.github.com>
2020-07-11 13:02:30 +0300

    Add files via upload

---
+++ week1/problem1_1.py
@@ -0,0 +1,15 @@
+'''Write a function problem1_1() that prints "Problem Set 1". 
+
+   Tip: Be careful that your program outputs this exact phrase with no additional 
+    characters.  It will be graded automatically and must be precise.  Here is the
+    run of my solved problem 1_1:
+
+    problem1_1()
+    Problem Set 1
+'''
+
+def problem1_1():
+
+    print("Problem Set 1")
+
+#problem1_1
---
+++ week1/problem1_2.py
@@ -0,0 +1,15 @@
+"""
+Problem 1_2:
+Write a function problem1_2(x,y) that prints the sum and product of the
+numbers x and y on separate lines, the sum printing first.
+"""
+
+def problem1_2(x,y):
+
+  
+    print(str(x + y))
+    print(str(x * y))
+
+
+
+#problem1_2(4, 5)
